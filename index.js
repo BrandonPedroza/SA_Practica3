918a10ff99b6d5bb378c0da896016e0f52c9067b
Brandon Pedroza <pato094@live.com>
2021-09-27 04:53:36 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 //respuesta a "/"
 app.get('/', function (req, res) {
- res.send('HELLO WORLD FOR SA :D, esta es la practica 5');
+ res.send('HELLO WORLD FOR SA :D, esta es la practica 5***');
 });
 //puesto 3000 por defecto
 app.listen(process.env.PORT || 3000);
