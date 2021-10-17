db6b5417419bad98d13ffebf0a97dac210f1d21f
Brandon Pedroza <pato094@live.com>
2021-10-17 02:51:52 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 //respuesta a "/"
 app.get('/', function (req, res) {
- res.send('***PRACTICA 6 - SOFTWARE AVANZADO***');
+ res.send('***PRACTICA 6 - SOFTWARE AVANZADO------cambio  para video***');
 });
 //puesto 3000 por defecto
 app.listen(process.env.PORT || 3000);
